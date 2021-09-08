bb112d2e7af1aeecf4f2638252f71461bf767fd3
vood <balitsky_igor@list.ru>
2021-09-08 21:46:58 +0300

    q

--- view-nt/src/main/java/ru/vood/generator/forth/runner/RunnerService.kt
+++ view-nt/src/main/java/ru/vood/generator/forth/runner/RunnerService.kt
@@ -7,9 +7,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import ru.vood.generator.datamodel.score.ScoreFunctionalDto
-import ru.vood.generator.forth.prop.GenerationProperties
 import java.time.LocalDateTime
-import java.util.*
 
 @Service
 class RunnerService(
@@ -20,22 +18,22 @@ class RunnerService(
     lateinit var host: String
 
     @Value("\${gen.cnt}")
-     var cnt: Int = 2
+    var cnt: Int = 2
 
     private val log = LogFactory.getLog(RunnerService::class.java)
 
     private val restTemplate: RestTemplate by lazy { restTemplateBuilder.build() }
 
     override fun run(vararg args: String?) {
-        val   now: LocalDateTime = LocalDateTime.now()
+        val now: LocalDateTime = LocalDateTime.now()
         IntRange(1, cnt).toList().parallelStream()
             .forEach {
                 val forObject =
                     restTemplate.getForObject("http://$host/score/$it", ScoreFunctionalDto::class.java)
                 log.info(forObject)
             }
-        val sec : Double = (LocalDateTime.now().nano - now.nano).toDouble()/1000000000
-        log.info("per score ${sec/cnt.toDouble()} sec")
+        val sec: Double = (LocalDateTime.now().nano - now.nano).toDouble() / 1000000000
+        log.info("per score ${sec / cnt.toDouble()} sec")
 
     }
 }
