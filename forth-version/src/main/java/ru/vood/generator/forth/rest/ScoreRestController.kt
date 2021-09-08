867b34f7ee0019836ad136984a7ee9d724befddf
vood <balitsky_igor@list.ru>
2021-09-08 19:54:35 +0300

    q

--- forth-version/src/main/java/ru/vood/generator/forth/rest/ScoreRestController.kt
+++ forth-version/src/main/java/ru/vood/generator/forth/rest/ScoreRestController.kt
@@ -1,5 +1,6 @@
 package ru.vood.generator.forth.rest
 
+import org.apache.commons.logging.LogFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -9,8 +10,11 @@ import ru.vood.generator.datamodel.score.ScoreFunctionalDto
 @RestController
 class ScoreRestController : EntityRestControllerService<ScoreFunctionalDto> {
 
+    private val log = LogFactory.getLog(ScoreRestController::class.java)
+
     @GetMapping(value = ["/score/{entityId}"])
     override fun readEntity(@PathVariable entityId: String): ScoreFunctionalDto {
+        log.info("entityId => $entityId")
         return ScoreFunctionalDto(entityId)
     }
 
