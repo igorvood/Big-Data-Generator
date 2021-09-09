4b446bd86ee7a13e307df0a336716a98061d4dec
vood <balitsky_igor@list.ru>
2021-09-09 11:27:02 +0300

    q

---
+++ data-model/src/main/java/ru/vood/generator/datamodel/Util.kt
@@ -0,0 +1,34 @@
+package ru.vood.generator.datamodel
+
+import kotlin.reflect.KCallable
+import kotlin.reflect.KProperty
+import kotlin.reflect.KVisibility
+
+fun <T> valueCalculate(
+    fields: Set<KCallable<*>>,
+    property: KProperty<*>,
+    thisRef: T
+) = fields
+    .asSequence()
+    .filter { it.visibility != KVisibility.PRIVATE }
+    .filter { it.name != property.name }
+    .filter {
+        it.name !in listOf(
+            "component1",
+            "copy",
+            "equals",
+            "hashCode",
+            "toString"
+        )
+    }
+    .map {
+        try {
+            it.name to it.call(thisRef)
+        } catch (e: Exception) {
+            println(it.name)
+            throw e
+        }
+    }
+    .filter { it.second != null }
+    .toList()
+    .toMap()
--- data-model/src/main/java/ru/vood/generator/datamodel/score/ScoreValueMap.kt
+++ data-model/src/main/java/ru/vood/generator/datamodel/score/ScoreValueMap.kt
@@ -1,38 +1,18 @@
 package ru.vood.generator.datamodel.score
 
+import ru.vood.generator.datamodel.valueCalculate
 import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KCallable
 import kotlin.reflect.KProperty
 import kotlin.reflect.KVisibility
 
 class ScoreValueMap : ReadOnlyProperty<ScoreFunctionalDto, Map<String, Any?>> {
 
     override fun getValue(thisRef: ScoreFunctionalDto, property: KProperty<*>): Map<String, Any?> {
-        val fields = ScoreFunctionalDto.fields
+        val fields: Set<KCallable<*>> = ScoreFunctionalDto.fields
 
-        val map: Map<String, Any?> = fields
-            .asSequence()
-            .filter { it.visibility != KVisibility.PRIVATE }
-            .filter { it.name != property.name }
-            .filter {
-                !(it.name in listOf(
-                    "component1",
-                    "copy",
-                    "equals",
-                    "hashCode",
-                    "toString"
-                ))
-            }
-            .map {
-                try {
-                    it.name to it.call(thisRef)
-                } catch (e: Exception) {
-                    println(it.name)
-                    throw e
-                }
-
-            }.filter { it.second != null }
-            .toList()
-            .toMap()
+        val map: Map<String, Any?> = valueCalculate(fields, property, thisRef)
         return map
     }
+
 }
