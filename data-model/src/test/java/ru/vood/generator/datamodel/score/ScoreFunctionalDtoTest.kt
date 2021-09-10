50e2b8d56686ed445335c28dd6e15f9900ed903e
vood <balitsky_igor@list.ru>
2021-09-10 20:40:00 +0300

    tests

--- data-model/src/test/java/ru/vood/generator/datamodel/score/ScoreFunctionalDtoTest.kt
+++ data-model/src/test/java/ru/vood/generator/datamodel/score/ScoreFunctionalDtoTest.kt
@@ -89,29 +89,31 @@ internal class ScoreFunctionalDtoTest {
     }
 
     companion object {
-        val fields = mapOf(
-            "cindex" to 1,
-            "objectInMap" to 1,
-            "clu" to 1,
-            "cluParticipants" to 1,
-            "id" to 1,
-            "merSign" to 1,
-            "mshFlg" to 1,
-            "nonCurAssets" to 1,
-            "opkFlag" to 1,
-            "overCap" to 1,
-            "ratingOffline" to 1,
-            "ratingOfflinePrice" to 1,
-            "ratingOfflineReserve" to 1,
-            "riskSegmentOffline" to 1,
-            "riskSegmentOfflineDate" to 1,
-            "skeBase" to 1,
-            "skeBcCap" to 1,
-            "skeD0" to 1,
-            "skeOffline" to 1,
-            "thmSign" to 1,
-            "wsRatingRestr" to 1,
+        val fields by lazy {
+            mapOf(
+                "cindex" to 1,
+                "objectInMap" to 1,
+                "clu" to 1,
+                "cluParticipants" to 1,
+                "id" to 1,
+                "merSign" to 1,
+                "mshFlg" to 1,
+                "nonCurAssets" to 1,
+                "opkFlag" to 1,
+                "overCap" to 1,
+                "ratingOffline" to 1,
+                "ratingOfflinePrice" to 1,
+                "ratingOfflineReserve" to 1,
+                "riskSegmentOffline" to 1,
+                "riskSegmentOfflineDate" to 1,
+                "skeBase" to 1,
+                "skeBcCap" to 1,
+                "skeD0" to 1,
+                "skeOffline" to 1,
+                "thmSign" to 1,
+                "wsRatingRestr" to 1,
 
-            )
+                )
+        }
     }
 }
