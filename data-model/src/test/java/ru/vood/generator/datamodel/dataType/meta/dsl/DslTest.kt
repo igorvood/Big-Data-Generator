c7179f9fca92df6a0d8a8f42d502b2d7d09bbb24
vood <balitsky_igor@list.ru>
2021-09-18 17:58:32 +0300

    new idea

--- data-model/src/test/java/ru/vood/generator/datamodel/dataType/meta/dsl/DslTest.kt
+++ data-model/src/test/java/ru/vood/generator/datamodel/dataType/meta/dsl/DslTest.kt
@@ -22,15 +22,18 @@ internal class DslTest {
     fun tearDown() {
     }
 
+    private val GEN_STR =
+        object : GenerateFieldValueFunction<ScoreDto, String?> {
+            override fun invoke(entityTemplate: EntityTemplate<ScoreDto>, s: String): DataType<String?> {
+                return StringType ("${entityTemplate.id}_$s" )
+            }
+        }
+
     @Test
     fun getName() {
         val string = MetaProperty(
             "riskSegmentOffline",
-            object : GenerateFieldValueFunction<ScoreDto, String?> {
-                override fun invoke(entityTemplate: EntityTemplate<ScoreDto>, s: String): DataType<String?> {
-                    return StringType { "${entityTemplate.id}_$s" }
-                }
-            }
+            GEN_STR
         )
         val number = MetaProperty(
             "number",
@@ -39,7 +42,7 @@ internal class DslTest {
                     entityTemplate: EntityTemplate<ScoreDto>,
                     s: String
                 ): DataType<BigDecimal?> {
-                    return NumberType { BigDecimal(entityTemplate.id.hashCode() + s.hashCode()) }
+                    return NumberType ( BigDecimal(entityTemplate.id.hashCode() + s.hashCode()) )
                 }
             })
         val date = MetaProperty(
@@ -49,11 +52,11 @@ internal class DslTest {
                     entityTemplate: EntityTemplate<ScoreDto>,
                     s: String
                 ): DataType<LocalDateTime?> {
-                    return DateType {
+                    return DateType (
                         LocalDateTime
                             .of(1970, 1, 1, 12, 12)
                             .plusSeconds(entityTemplate.id.hashCode().toLong() + s.hashCode().toLong())
-                    }
+                    )
                 }
             })
         val expected: MetaEnt<ScoreDto> = MetaEnt(
@@ -65,13 +68,13 @@ internal class DslTest {
             ),
         )
 
-        val score: MetaEnt<ScoreDto> by entity<ScoreDto> {
-            val riskSegmentOffline by string()  //withFun {}//NOT NULL
+        val score by entity<ScoreDto> {
+            val riskSegmentOffline by string() withFun { et, pn -> StringTypeNotNull { "${et.id}_$pn" } }
             val number by number()
             val date by date()
 
-//            val client by ref<ScoreDto>()
-//            riskSegmentOffline withFun {}
+            val refOtherEnt by ref<ScoreDto>()
+            val setOtherEnt by set<ScoreDto>()
         }
 
         assertEquals(expected, score)
